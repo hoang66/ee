ce4a018b5cc2eabe20d64dc734bcca2ab590bd1c
hoang66 <nguyenleeduchoangvn@gmail.com>
2022-06-23 17:25:49 +0700

    h

---
+++ my.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE my_database7;
+create table my_database7.hocsinh(
+
+ name varchar(300),
+tuoi int,
+primary key(tuoi)
+);
+
+insert into my_database7.hocsinh value('hoang',17);
+delete from hocsinh where tuoi=18;
